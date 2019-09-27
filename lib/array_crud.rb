d8e700698e32e919fd7e38a612eaffb1cd129dbc
Rose Seraphin <roseseraphin@yahoo.com>
2019-09-27 00:18:59 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,51 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  favorite_restaurants = ["Seasons 52", "Cheesecake Factory", "Ruth's Chris", "Red Lobter"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+
+element = "arrays!"
+array << "arrays!"
+
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  element = "wow"
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+ arrays = array.pop
+ 
 end
 
 def remove_element_from_start_of_array(array)
+  wow = array.shift 
   
 end
 
 def retrieve_element_from_index(array, index_number)
+   array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array[2]
   
-end
+  end
 
 def retrieve_first_element_from_array(array)
   
+ array = ["wow", "I", "am", "really", "learning", "arrays!"]
+array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
+array[4] = "totally"
 
 end
